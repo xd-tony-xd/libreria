46438caad6dd5db143af251977d6423e313710d9
Diego060410 <diegoluis0604@gmail.com>
2024-12-12 22:57:07 -0500

    Create Facturas.java

---
+++ src/main/java/com/proyec/libreria/entity/Facturas.java
@@ -0,0 +1,24 @@
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "proveedores")
+@Getter
+@Setter
+public class Facturas {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_factura;
+
+    @Column(name = "NOMBRE_PROVEEDOR", nullable = false)
+    private String id_usuario;
+
+    @Column(name = "TELEFONO", nullable = false, unique = true)
+    private String fecha_factura; 
+}
